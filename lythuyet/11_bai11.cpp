95c34afc21b1e32d5795561055b79df22edd2ed4
tuanldda06 <leducanhtuan006@gmail.com>
2025-08-14 22:27:26 +0700

    Thembai11

---
+++ lythuyet/11_bai11.cpp
@@ -0,0 +1,3 @@
+//Để khai bào hằng số trong c++ , sử dụng từ khóa const trước hoặc sau kiểu dữ liệu của biến và phải được khởi tạo trong lúc khai báo
+const double pi{3.14159}; // hay dùng
+double const speed_sound{343.2}; // ít dùng
