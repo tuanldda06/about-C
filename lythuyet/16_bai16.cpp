98863e937b4496a9911473332c931fc0408f381f
tuanldda06 <leducanhtuan006@gmail.com>
2025-08-15 12:34:37 +0700

    Thembai16

--- lythuyet/16_bai16.cpp
+++ lythuyet/16_bai16.cpp
@@ -1,1 +1,18 @@
+//Biến toán cục : được khai báo bên ngoài khối lệnh , có thể truy cập và sử dụng ở bất ksy đâu trong file chứa biến đó
+#include <iostream>
+using namespace std;
+int g_x;
+const int g_y(2); // hằng số
+void doSomething()
+{
+  g_x=3;
+  cout<<g_y<<'\n\';
+}
+int main(0
+{
+  doSomething();
+  g_x =5;
+  cout<<g_y<<'\n';
+  return 0;
+}
 
